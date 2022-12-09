bdb7bed4fb7487d2576887d7b9b3a65caf2bc765
Benjamin Haag <b3ny4@protonmail.com>
2022-12-09 20:13:52 +0100

    Hello World

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rootkit
+Simple Hello World LKM
+
+## Build
+```
+$ make
+```
+
+## Install
+```
+$ sudo insmod rootkit.ko
+```
+
+## uninstall
+```
+$ sudo rmmod rootkit
+```
---
+++ rootkit.c
@@ -0,0 +1,29 @@
+#include <linux/module.h>
+
+#define LICENCE "GPL"
+#define DESCRIPTION "Rootkit that can hide arbitrary ressources by using the configure command line tool."
+#define AUTHOR "Benjamin Haag"
+#define VERSION "0.1"
+
+MODULE_LICENSE(LICENCE);
+MODULE_DESCRIPTION(DESCRIPTION);
+MODULE_AUTHOR(AUTHOR);
+MODULE_VERSION(VERSION);
+
+
+
+static int __init
+module_load(void) {
+    printk(KERN_INFO "Rootkit: loaded. Hello!\n");
+    return 0;
+}
+
+
+static void __exit
+module_unload(void) {
+    printk(KERN_INFO "Rootkit: unloaded. Bye Bye!\n");
+}
+
+
+module_init(module_load);
+module_exit(module_unload);
